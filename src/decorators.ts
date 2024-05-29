138a77e9a0596de59ac3820e8d38742a239d50d6
RaylanGalvao <153932047+RaylanGalvao@users.noreply.github.com>
2024-05-29 10:52:47 -0300

    estudos com decorators

---
+++ src/decorators.ts
@@ -0,0 +1,40 @@
+/*
+decorator
+classes 
+propriedades
+metodos
+parametros(bem pouco usado)
+getters / setters (bem pouco usado)
+*/
+
+// function logInfo(target: any){
+//     console.log("Sistema esta online")
+// }
+
+// function logInfo(mensagem: string){
+//     return (target: any)=>{
+//         console.log(`${mensagem}, ${target}`)
+//     }
+// }
+
+// @logInfo("Servidor esta rodando")
+// class sistema{
+
+// }
+
+//praticando //
+
+
+function setIpServidor(novoIp: string){
+    return (target: any) => {//target e o construtor da nossa classe
+        return class extends target{
+            ip = novoIp
+        }
+    }
+}
+@setIpServidor("192.168.50.30")
+class Servidor{
+}
+
+const servidor1 =  new Servidor()
+console.log(servidor1)
